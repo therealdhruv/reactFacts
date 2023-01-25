fbd032661c3eafe468c3eadd0e9daac398585829
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-01-26 00:09:46 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <>
             <nav>
-                <img src={require("../images/react-icon-small.png")} className="nav--icon" />
+                <img src={ require("../images/react-icon-small.png") } className="nav--icon" />
                 <h3 className="nav--iconText" > ReactFacts </h3>
                 <h4 className="nav--rightText" > ReactCouse Project-1 </h4>
             </nav>
